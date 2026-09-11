c215fe92253269cf9b455ea6c82a7a17ac01c37b
kartoevyakoub-sys <kartoevyakoub-sys@users.noreply.github.com>
2026-09-11 22:47:45 +0300

    Remove debug helper

--- _inspect.mjs
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-const source = fs.readFileSync('node_modules/@vercel/blob/dist/chunk-YYMLUMXS.js', 'utf8');
-for (const key of ['parseBody', 'computeBodyLength', 'isStream', 'createUploadPart', 'parseReadableStream', 'requestApi(', 'async function put']) {
-  const i = source.indexOf(key);
-  console.log(key, '=>', i);
-  if (i >= 0) console.log('--- ctx ---\n' + source.slice(i, i + 500) + '\n');
-}
